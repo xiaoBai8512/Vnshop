290f68eca93526183402bc397ff2053aa3b1d229
xiaoBai8512 <851285142@qq.com>
2017-10-18 20:10:58 +0800

    ing

--- client/config/index.js
+++ client/config/index.js
@@ -35,7 +35,7 @@ module.exports = {
       //   target: 'http://localhost:3000'
       // },
       '/api/**': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:4000',
         pathRewrite: {
           '^/api': '/'
         }
